f80adc905bfbcf01f206a0e6b93a368d11d3f24e
Reid Williams <reid@computable.io>
2023-09-18 14:24:48 -0700

    add ball graphics and fix indentation

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ playdate.startAccelerometer()
 
 local sensitivity = {2.0, 2.0}
 
-local image = gfx.image.new("images/circle.png")
+local image = gfx.image.new("images/ball1.png")
 local sprite = playdate.graphics.sprite.new(image)
 sprite:moveTo(100, 100)
 sprite:add()
@@ -21,44 +21,44 @@ local sound = playdate.sound.sampleplayer.new("/sounds/beep.wav")
 
 -- Main game loop
 function playdate.update()
-   updateSprite()
-   playdate.graphics.sprite.update()
+    updateSprite()
+    playdate.graphics.sprite.update()
 end
 
 function updateSprite()
-   local ax, ay, az = playdate.readAccelerometer()
+    local ax, ay, az = playdate.readAccelerometer()
 
-   local friction = 0.95
+    local friction = 0.95
    
-   spriteVelocity[1] = (spriteVelocity[1] + ax * sensitivity[1]) * friction
-   spriteVelocity[2] = (spriteVelocity[2] + ay * sensitivity[2]) * friction
+    spriteVelocity[1] = (spriteVelocity[1] + ax * sensitivity[1]) * friction
+    spriteVelocity[2] = (spriteVelocity[2] + ay * sensitivity[2]) * friction
       
-   local x = sprite.x + spriteVelocity[1]
-   if x > 400 or x < 0 then
-      spriteVelocity[1] = -spriteVelocity[1]
-      sound:play(1)
-   end
-   x = clip(x, 0, 400)
+    local x = sprite.x + spriteVelocity[1]
+    if x > 400 or x < 0 then
+        spriteVelocity[1] = -spriteVelocity[1]
+        sound:play(1)
+    end
+    x = clip(x, 0, 400)
     
-   local y = sprite.y + spriteVelocity[2]
-   if y > 240 or y < 0 then
-      spriteVelocity[2] = -spriteVelocity[2]
-      sound:play(1)
-   end
-   y = clip(y, 0, 240)
+    local y = sprite.y + spriteVelocity[2]
+    if y > 240 or y < 0 then
+        spriteVelocity[2] = -spriteVelocity[2]
+        sound:play(1)
+    end
+    y = clip(y, 0, 240)
    
-   sprite:moveTo(x, y)
+    -- sprite:moveTo(x, y)
 end
 
 function clip(val, min, max)
-   if val > max then return max end
-   if val < min then return min end
-   return val
+    if val > max then return max end
+    if val < min then return min end
+    return val
 end
 
 function round(num, decimalPlaces)
-   local mult = 10^(decimalPlaces or 1)
-   return math.floor(num * mult + 0.5) / mult
+    local mult = 10^(decimalPlaces or 1)
+    return math.floor(num * mult + 0.5) / mult
 end
 
 
